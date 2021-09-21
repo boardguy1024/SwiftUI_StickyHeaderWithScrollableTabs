0cd47d8204c9e293c2014a55d4c4aae519f5a98a
boardguy1024 <boardguy1024@gmail.com>
2021-09-21 10:54:14 +0900

    Initial Commit

---
+++ SwiftUI_StickyHeaderWithScrollableTabs/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_StickyHeaderWithScrollableTabs
+//
+//  Created by park kyung seok on 2021/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_StickyHeaderWithScrollableTabs/SwiftUI_StickyHeaderWithScrollableTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StickyHeaderWithScrollableTabsApp.swift
+//  SwiftUI_StickyHeaderWithScrollableTabs
+//
+//  Created by park kyung seok on 2021/09/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StickyHeaderWithScrollableTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
